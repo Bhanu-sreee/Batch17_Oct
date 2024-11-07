ef627fd6cb3f9b3012c731e0bf519ea96d49b672
Bhanu-sreee <nellorebhanusreevce@gmail.com>
2024-11-07 22:16:06 +0530

    Update a1.c

--- Day_10_A/a1.c
+++ Day_10_A/a1.c
@@ -1,26 +1,44 @@
+/* DESCRIPTION : Program to find the number can be expressed as the sum of two prime number
+Date : 7-11-2024
+Name : Nellore Bhanusree
+*/
+
 #include <stdio.h>
-// Function to check  prime number
-int is_prime(int n)
+int is_prime(int n);
+int primePairs(int n);
+
+int main() 
 {
-    if (n<=1); 
-       
-    for (int i=2; i*i<=n; i++)
-    {
-        if (n%i==0)
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    primePairs(n);
+    return 0;
+}
+
+  int is_prime(int n)
+{
+  {
+    if (n<=1) 
         return 0;
-    }
+  }
+    for (int i=2; i*i<=n; i++)
+  {
+        if (n%i==0)
+            return 0;
+  }
     return 1;
 }
 
-// Function for prime numbers sum
-int pairs(int num)
+
+int primePairs(int n)
 {
     int sum=0;
-    for (int i=2; i<=num/2; i++) 
+    for (int i=2; i<=n/2; i++)
     {
-        if (is_prime(i) && is_prime(num-i)) 
+        if (is_prime(i) && is_prime(n-i)) 
         {
-            printf("%d=%d+%d\n",num,i,num-i);
+            printf("%d=%d+%d\n",n,i,n-i);
             sum++;
         }
     }
@@ -32,13 +50,4 @@ int pairs(int num)
     {
         printf("No of Ways = -1\n");
     }
-}
-
-int main() 
-{
-    int num;
-    printf("Enter a positive integer: ");
-    scanf("%d", &num);
-    pairs(num);
-    return 0;
 }
